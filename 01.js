56a42e43429de22843532356a63d8048bbe38b7d
unknown <67726628+rohitscript@users.noreply.github.com>
2024-08-12 12:22:21 +0530

    Added Day01-CreateHelloWorld

---
+++ 01.js
@@ -0,0 +1,8 @@
+//Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
